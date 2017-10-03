1785bf83b54a12f166f5603bb08b04a2472cc835
WNjihia <dorisn172@gmail.com>
2017-10-03 16:51:14 +0300

    add send email and slack notifications constants

---
+++ ckanext-notify/ckanext/notify/constants.py
@@ -0,0 +1,4 @@
+from ckanext.datarequests.constants import *
+
+DATAREQUEST_SEND_EMAIL_NOTIFICATION = 'datarequest_email_notification'
+DATAREQUEST_SEND_SLACK_NOTIFICATION = 'datarequest_send_slack_notification'
