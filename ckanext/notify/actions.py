d46c6cdfb12ebf31fdd32c9f1ed3be2d606590ef
WNjihia <dorisn172@gmail.com>
2017-10-16 15:17:21 +0300

    add update show and delete actions

--- ckanext/notify/actions.py
+++ ckanext/notify/actions.py
@@ -150,6 +150,176 @@ def datarequest_register_email(context, data_dict):
     return _dictize_email_details(email_details)
 
 
+def email_channel_show(context, data_dict):
+    '''
+    Action to retrieve the information of an email notification channel.
+    The only required parameter is the id of the channel. A NotFound
+    exception will be risen if the given id is not found.
+    Access rights will be checked before returning the information and an
+    exception will be risen (NotAuthorized) if the user is not authorized.
+    :param context: the context of the request
+    :type data_dict: dict
+    :param data_dict: Contains the following
+    id: The id of the email notification channel to be shown
+    :type data_dict: dict
+    :returns: A dict with the data request (id, user_id, title, description,
+        organization_id, open_time, accepted_dataset, close_time, closed)
+    :rtype: dict
+    '''
+
+    model = context['model']
+    id = data_dict.get('id', '')
+
+    if not id:
+        raise tk.ValidationError(tk._('Channel ID has not been included'))
+
+    # Init the data base
+    db.init_db(model)
+
+    # Check access
+    tk.check_access(constants.DATAREQUEST_REGISTER_EMAIL, context, data_dict)
+
+    # Get the data request
+    result = db.Org_Email_Details.get(id=id)
+    if not result:
+        raise tk.ObjectNotFound(tk._('Channel {0} not found in the data base'.format(id)))
+
+    email_data = result[0]
+    data_dict = _dictize_email_details(email_data)
+
+    return data_dict
+
+
+def email_channels_show(context, data_dict):
+    '''
+    Action to retrieve the email notification channels. The only required
+    parameter is the name of the organization passed in as an id. A NotFound
+    exception will be risen if the organization id is not found.
+    Access rights will be checked before returning the information and an
+    exception will be risen (NotAuthorized) if the user is not authorized.
+    :param context: the context of the request
+    :type data_dict: dict
+    :param data_dict: Contains the following
+    organization_id: The ID of the organization
+    :type data_dict: dict
+    :returns: A list of the email notification details(id,
+        organization_id, email)
+    :rtype: list
+    '''
+
+    model = context['model']
+    organization_id = data_dict.get('organization_id', '')
+
+    if not organization_id:
+        raise tk.ValidationError(tk._('Organization ID has not been included'))
+
+    # Init the data base
+    db.init_db(model)
+
+    # Check access
+    tk.check_access(constants.DATAREQUEST_REGISTER_EMAIL, context, data_dict)
+
+    # Get the available slack channels
+    result = db.Org_Email_Details.get(organization_id=organization_id)
+    if result:
+        email_channels = [_dictize_email_details(channel) for channel in result]
+    else:
+        email_channels = {}
+
+    return email_channels
+
+
+def email_channel_update(context, data_dict):
+    '''
+    Action to update an email notification channel. The only required parameter is the id
+    of the channel. The function checks the access rights of the user before
+    updating the email notification channel. If the user is not allowed a NotAuthorized
+    exception will be risen.
+    In addition, you should note that the parameters will be checked and an
+    exception (ValidationError) will be risen if some of these parameters are
+    invalid.
+    :param context: the context of the request
+    :type data_dict: dict
+    :param data_dict: Contains the following:
+    id: The ID of the email notification channel to be updated
+    email: The email address of the organization
+    organization_id: The ID of the organization whose channel is to be updated
+    :type data_dict: dict
+    :returns: A dict with the data request (id, email,
+        organization_id)
+    :rtype: dict
+    '''
+
+    model = context['model']
+    session = context['session']
+    id = data_dict.get('id', '')
+
+    if not id:
+        raise tk.ValidationError(tk._('Email Notification Channel ID has not been included'))
+
+    # Init the data base
+    db.init_db(model)
+
+    # Check access
+    tk.check_access(constants.DATAREQUEST_REGISTER_EMAIL, context, data_dict)
+
+    # Get the initial data
+    result = db.Org_Email_Details.get(id=id)
+    if not result:
+        raise tk.ObjectNotFound(tk._('Channel {0} not found in the database'.format(id)))
+    email_data = result[0]
+
+    # Avoid the validator to return an error when the user does not change the title
+    # context['avoid_existing_title_check'] = data_req.title == data_dict['title']
+
+    # Validate data
+    validator.validate_email_form(context, data_dict)
+
+    # Set the data provided by the user in the data_red
+    _undictize_email_basic(email_data, data_dict)
+
+    session.add(email_data)
+    session.commit()
+
+    return _dictize_email_details(email_data)
+
+
+def email_channel_delete(context, data_dict):
+    '''
+    Action to delete an email notification channel. The function checks the access rights
+    of the user before deleting the data request. If the user is not allowed
+    a NotAuthorized exception will be risen.
+    :param context: the context of the request
+    :type data_dict: dict
+    :param data_dict: Contains the following
+    id: The id of the slack notification channel to delete
+    :type data_dict: dict
+    '''
+
+    model = context['model']
+    session = context['session']
+    id = data_dict.get('id', '')
+
+    # Check id
+    if not id:
+        raise tk.ValidationError(tk._('Slack Channel ID has not been included'))
+
+    # Init the data base
+    db.init_db(model)
+
+    # Check access
+    tk.check_access(constants.DATAREQUEST_REGISTER_EMAIL, context, data_dict)
+
+    # Get the slack channel
+    result = db.Org_Email_Details.get(id=id)
+    if not result:
+        raise tk.ObjectNotFound(tk._('Channel {0} not found in the database'.format(id)))
+
+    email_data = result[0]
+    session.delete(email_data)
+    session.commit()
+
+
 def send_slack_message(slack_data):
     response = requests.post(
         config.get('slack.webhook_url'), data=json.dumps(slack_data),
